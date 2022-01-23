a8b06a141889a5de8b175c139223326efa7413d5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-23 22:13:15 +0100

    KIOO 3

--- OpenWeather.kt
+++ OpenWeather.kt
@@ -1,10 +1,12 @@
 //
 class OpenWeather {
 
+    //
     public fun OpenWeather(localization: String, countryCode: String, apiKey: String) {
 
     }
 
+    //
     public fun OpenWeather(localization: String, apiKey: String) {
 
     }
