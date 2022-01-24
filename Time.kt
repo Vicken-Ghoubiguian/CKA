148cba5d4e942fca8a8bef9ed993525f34f1c09e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-24 18:40:09 +0100

    For constrFT_2

--- Time.kt
+++ Time.kt
@@ -1,16 +1,19 @@
 class Time {
 
     //
-    private var sunriseAsTimestampAccordingToUtc: String? = null
-    private var sunsetAsTimestampAccordingToUtc: String? = null
-    private var utcOffsetAsTimestamp: String? = null
+    private var sunriseAsTimestampAccordingToUtc: Long? = null
+    private var sunsetAsTimestampAccordingToUtc: Long? = null
+    private var utcOffsetAsTimestamp: Short? = null
 
     //
-    public fun Time(sunriseAsTimestampAccordingToUtc: String, sunsetAsTimestampAccordingToUtc: String, utcOffsetAsTimestamp: String) {
+    public fun Time(sunriseAsTimestampAccordingToUtc: Long, sunsetAsTimestampAccordingToUtc: Long, utcOffsetAsTimestamp: Short) {
 
         //
         this.sunriseAsTimestampAccordingToUtc = sunriseAsTimestampAccordingToUtc
         this.sunsetAsTimestampAccordingToUtc = sunsetAsTimestampAccordingToUtc
         this.utcOffsetAsTimestamp = utcOffsetAsTimestamp
     }
+
+    //
+    //public fun getSunriseAsTimestampAccordingToUtc
 }
