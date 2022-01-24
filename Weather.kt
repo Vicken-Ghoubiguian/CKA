ce40e6b24eeb3e1cb2ad70c68163ebaca641926a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-24 17:43:57 +0100

    W<X>W 3

---
+++ Weather.kt
@@ -0,0 +1,9 @@
+//
+final class Weather {
+
+    //
+    private var id: Int = 0
+    private var main: String = ""
+    private var description: String = ""
+    private var iconUrl: String = ""
+}
