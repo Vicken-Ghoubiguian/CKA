0b2a276226a144c781e528bb8c54b33aea8672dd
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-25 09:52:31 +0100

    Adding class 'OpenWeather'

---
+++ OpenWeather.kt
@@ -0,0 +1,13 @@
+//
+class OpenWeather {
+
+    //
+    public fun OpenWeather(localization: String, apiKey: String) {
+
+    }
+
+    //
+    public fun OpenWeather(localization: String, countryCode: String, apiKey: String) {
+        
+    }
+}
