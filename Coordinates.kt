b41429496f5eb21c1f813897880eae466bd19088
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-25 08:44:44 +0100

    Adding all

---
+++ Coordinates.kt
@@ -0,0 +1,36 @@
+//
+final class Coordinates {
+
+    //
+    private var longitude: Double? = null
+    private var latitude: Double? = null
+
+    //
+    public fun Coordinates(longitude: Double, latitude: Double) {
+
+        //
+        this.longitude = longitude
+        this.latitude = latitude
+    }
+
+    //
+    public fun getLongitude(): Double? {
+
+        //
+        return this.longitude
+    }
+
+    //
+    public fun getLatitude(): Double? {
+
+        //
+        return this.latitude
+    }
+
+    //
+    public override fun toString(): String {
+
+        //
+        return ""
+    }
+}
