695757fc9a6c99f26f4a99d639cd815c5fc655db
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-25 12:25:28 +0100

    Temp 2..

--- src/main/kotlin/openWeather/Temperature.kt
+++ src/main/kotlin/openWeather/Temperature.kt
@@ -7,4 +7,11 @@ final class Temperature {
     private var value: Double? = null
     private var measureUnit: TemperatureUnitEnum? = null
 
+    //
+    public fun Temperature(value: Double) {
+
+        //
+        this.value = value
+        this.measureUnit = TemperatureUnitEnum.KELVIN
+    }
 }
