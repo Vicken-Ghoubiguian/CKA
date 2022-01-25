a54a3d034014f9f6e5ad9704849b73852d97540b
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-25 10:17:32 +0100

    For 'Coordinates'

--- src/main/kotlin/OpenWeather/Coordinates.kt
+++ src/main/kotlin/OpenWeather/Coordinates.kt
@@ -1,4 +1,17 @@
 package OpenWeather
 
+//
 class Coordinates {
+
+    //
+    var longitude: Double? = null
+    var latitude: Double? = null
+
+    //
+    public fun Coordinates(longitude: Double, latitude: Double) {
+
+        //
+        this.longitude = longitude
+        this.latitude = latitude
+    }
 }
