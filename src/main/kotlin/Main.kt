74fb7bbd0409bcb7d47e17dbadefdefa46dec771
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-27 19:00:32 +0100

    It's 'Johnny' 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,10 @@ import presentopenWeather.PresentOpenWeather
 fun main(args: Array<String>) {
 
     //
-    var currentOpenWeather = PresentOpenWeather("", "", "")
+    var currentPresentOpenWeather = PresentOpenWeather("", "", "")
 
     //
-    println(currentOpenWeather.toString())
+    println(currentPresentOpenWeather.toString())
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
