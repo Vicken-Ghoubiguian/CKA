748321aa5d819c06eca3ce93620e19dcb821dcc6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-28 11:03:02 +0100

    New class 'Wind'

--- src/main/kotlin/openWeather/Wind.kt
+++ src/main/kotlin/openWeather/Wind.kt
@@ -1,4 +1,42 @@
 package openWeather
 
+//
 class Wind {
+
+    //
+    private var speed: Double? = null
+    private var deg: Double? = null
+    private var gust: Double? = null
+
+    //
+    public fun Wind(speed: Double, deg: Double, gust: Double) {
+
+        this.speed = speed
+        this.deg = deg
+        this.gust = gust
+    }
+
+    //
+    public fun getSpeed(): Double? {
+
+        return this.speed
+    }
+
+    //
+    public fun getDeg(): Double? {
+
+        return this.deg
+    }
+
+    //
+    public fun getGust(): Double? {
+
+        return this.gust
+    }
+
+    //
+    public override fun toString(): String {
+
+        return ""
+    }
 }
