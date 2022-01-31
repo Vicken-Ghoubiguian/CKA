01f7b0b5a0c00606c2d712a46c4c3d0392efdeb9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-31 19:36:40 +0100

    JKLO 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:1.6.7")
+    implementation("io.ktor:ktor-client-cio:1.6.7")
 }
 
 tasks.test {
--- src/main/kotlin/openWeather/PresentOpenWeather.kt
+++ src/main/kotlin/openWeather/PresentOpenWeather.kt
@@ -1,6 +1,12 @@
 package openWeather
 
 //
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+//
 class PresentOpenWeather(localization: String, countryCode: String, apiKey: String) {
 
     //
