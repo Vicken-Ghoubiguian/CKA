316a7b1ccc9690a18ab5d4ec500af4a5f00bc2a5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-05 10:18:46 +0100

    Djarkha

--- src/main/kotlin/openWeather/GeographicLocation.kt
+++ src/main/kotlin/openWeather/GeographicLocation.kt
@@ -1,4 +1,20 @@
 package openWeather
 
 class GeographicLocation {
+
+    //
+    private var countryCode: String? = null
+    private var cityName: String? = null
+
+    //
+    public fun getCountryCode(): String? {
+
+        return this.countryCode
+    }
+
+    //
+    public fun getCityName(): String? {
+
+        return this.cityName
+    }
 }
