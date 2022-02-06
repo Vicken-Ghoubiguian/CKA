7fe2cd7160fbd6ec9653e2195fe3e13cea6288b3
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-06 12:17:47 +0100

    Ragnarok awaits...

--- src/main/kotlin/openWeather/PresentOpenWeather.kt
+++ src/main/kotlin/openWeather/PresentOpenWeather.kt
@@ -66,7 +66,7 @@ class PresentOpenWeather{
         } else {
 
             //
-
+            httpOpenWeatherRequest = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
         }
 
         //
