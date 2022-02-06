176f681571a4d913e7bb0132b9561ad5ce8a692f
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-06 19:31:23 +0100

    XHZJZJK

--- src/main/kotlin/openWeather/PresentOpenWeather.kt
+++ src/main/kotlin/openWeather/PresentOpenWeather.kt
@@ -15,6 +15,10 @@ class PresentOpenWeather{
     //
     private var coords: Coordinates? = null
     private var weather: Weather? = null
+    private var temperature: Temperature? = null
+    private var feelingLikeTemperature: Temperature? = null
+    private var minTemperature: Temperature? = null
+    private var maxTemperature: Temperature? = null
 
     private var humidity: Humidity? = null
     private var wind: Wind? = null
