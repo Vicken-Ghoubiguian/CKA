40c8be4c30113917bc5b76bab76a239f15f980b8
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-10 20:13:36 +0100

    Qzererr 4

--- src/main/kotlin/openWeather/ForecastOpenWeather.kt
+++ src/main/kotlin/openWeather/ForecastOpenWeather.kt
@@ -1,6 +1,13 @@
 package openWeather
 
 //
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.runBlocking
+
+//
 class ForecastOpenWeather {
 
     //
