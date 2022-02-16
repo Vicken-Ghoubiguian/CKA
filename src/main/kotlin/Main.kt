3d1c19302e2cf4aab727651515d1e11b92202a12
Wicken <ericghoubiguian@live.fr>
2022-02-16 21:51:33 +0100

    What a question ?...

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,5 +12,5 @@ suspend fun main(args: Array<String>) {
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // println("Program arguments: ${args.joinToString()}") ===> How to enter arguments to a Kotlin project ?
 }
